c68174a6362e2898df162d01412a7733e76daaf9
Paul Prince <paul@littlebluetech.com>
2014-01-29 07:48:01 -0500

    wip

--- config/compass/config.rb
+++ config/compass/config.rb
@@ -4,17 +4,15 @@ require 'susy'
 require 'sassy-buttons'
 
 http_path       = "/"
-sass_dir        = "sass"
+sass_dir        = "app/stylesheets"
 css_dir         = "_site/css"
 images_dir      = "images"
-fonts_dir       = "fonts"
+fonts_dir       = "app/fonts"
 http_fonts_path = "/fonts"
 relative_assets = false
-output_style    = :nested
 line_comments   = false
+output_style    = :nested
 environment     = :development
-# environment     = :production
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
